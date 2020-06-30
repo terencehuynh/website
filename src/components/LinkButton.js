3181055aaa1795a3f5fed7b04a2f46f4d76c2e8d
Terence Huynh <me@terencehuynh.com>
2020-07-01 00:48:49 +1000

    Improve link button styling with palette

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,31 +1,65 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link as GatsbyLink } from 'gatsby'
-import { Icon } from 'fannypack'
+import { Icon, styled, palette, css } from 'fannypack'
+
+const generateStyledLink = (component) => styled(component)`
+  text-decoration: none;
+  color: ${(props) => props.color};
+  transition: all 0.08s ease-in-out;
+  font-weight: bold;
+  text-transform: uppercase;
+  border-radius: 4px;
+  border: 1px solid ${(props) => props.color};
+  padding: 5px 7px;
+  display: inline-flex;
+  align-items: center;
+  font-size: 0.875rem;
+
+  svg {
+    margin-left: 4px;
+    transition: all 0.08s ease-in-out;
+  }
+
+  &:hover {
+    background-color: ${(props) => props.color};
+    color: white;
+
+    svg {
+      margin-left: 10px;
+    }
+  }
+
+  ${(props) =>
+    props.invert &&
+    css`
+      color: white;
+      border-color: white;
+
+      &:hover {
+        color: ${(props) => props.color};
+        background-color: white;
+      }
+    `}
+`
 
 const LinkButton = ({ text, to, activeClassName, invert, ...other }) => {
   const internal = /^\/(?!\/)/.test(to)
-  const className = invert ? 'line-button line-button__invert' : 'line-button'
   // Use Gatsby Link for internal links, and <a> for others
+  const component = internal ? GatsbyLink : 'a'
+  const StyledLink = generateStyledLink(component)
+  const props = { invert, ...other }
+
   if (internal) {
-    return (
-      <GatsbyLink
-        to={to}
-        className={className}
-        activeClassName={activeClassName}
-        {...other}
-      >
-        {text}
-        <Icon icon="solid-angle-right" />
-      </GatsbyLink>
-    )
+    Object.assign(props, { to, activeClassName })
+  } else {
+    Object.assign(props, { href: to })
   }
 
   return (
-    <a href={to} className={className} {...other}>
-      {text}
-      <Icon icon="solid-angle-right" />
-    </a>
+    <StyledLink {...props}>
+      {text} <Icon icon="solid-angle-right" />
+    </StyledLink>
   )
 }
 
@@ -33,12 +67,14 @@ LinkButton.propTypes = {
   text: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   activeClassName: PropTypes.string,
+  color: PropTypes.string,
   invert: PropTypes.bool,
 }
 
 LinkButton.defaultProps = {
   activeClassName: 'active',
   invert: false,
+  color: palette('primary'),
 }
 
 export default LinkButton
